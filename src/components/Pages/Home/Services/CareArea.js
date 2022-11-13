76647b9dc924adf6203b2d4d8a21802815aeb652
anowarzz <anowarhosen444@gmail.com>
2022-11-13 10:33:18 +0600

    adding our services

--- src/components/Pages/Home/Services/CareArea.js
+++ src/components/Pages/Home/Services/CareArea.js
@@ -4,15 +4,17 @@ import treatment from '../../../../assets/images/treatment.png'
 const CareArea = () => {
     return (
         <div>
-        <div className="hero">
-  <div className="hero-content flex-col justify-center lg:flex-row">
-    <div className=''>
-    <img src={treatment} alt="treatment" className="w-3/4 h-auto rounded-lg shadow-2xl" />
-    </div>
-    <div className='w-3/5'>
-      <h1 className="text-3xl md:text-5xl font-bold mb-6">Exceptional Dental <br className='hidden md:block'/> Care on Your Terms</h1>
-      <p className="text-justify md:w-4/5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+       <div className="hero">
+  <div className="hero-content flex-col lg:flex-row">
+  <div className=''>
+  <img src={treatment} alt="treatment" className="w-4/5 rounded-lg shadow-2xl" />
+  </div>
+    <div>
+   <div className='w-4/5'>
+   <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Get Started</button>
+   </div>
     </div>
   </div>
 </div>
