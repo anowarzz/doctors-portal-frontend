9fdb5dbc063afbe4be35a7d8abcf4394da88e8e6
anowarzz <anowarhosen444@gmail.com>
2022-11-25 12:37:49 +0600

    user make admin functionality implemented

--- src/components/Pages/DashBoard/AllUsers/AllUsers.js
+++ src/components/Pages/DashBoard/AllUsers/AllUsers.js
@@ -1,8 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useContext } from "react";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../../../contexts/AuthProvider";
 
 const AllUsers = () => {
-  const { data: users = [] } = useQuery({
+
+const {user} = useContext(AuthContext)
+
+console.log(user);
+
+
+
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/users");
@@ -11,9 +20,26 @@ const AllUsers = () => {
     },
   });
 
+  // Function to assign admin role to a user
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+       if(data.modifiedCount > 0){
+        toast.success("Admin Added Successfully");
+        refetch();
+       }
+      });
+  };
+
   return (
     <div>
-      <h2 className="text-3xl">All Users</h2>
+      <h2 className="text-3xl my-4 font-semibold px-2">All Users</h2>
 
       <div className="overflow-x-auto">
         <table className="table w-full">
@@ -22,18 +48,29 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
-              <th>Favorite Color</th>
-              <th>Favorite Color</th>
+              <th>Admin</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, i) => (
-              <tr  key={user?._id}>
-                <th>{i+1}</th>
+              <tr key={user?._id}>
+                <th>{i + 1}</th>
                 <td>{user?.name}</td>
                 <td>{user?.email}</td>
-                <td>Blue</td>
-                <td>Blue</td>
+                <td>
+                  {user?.role !== "admin" && (
+                    <button
+                      onClick={() => handleMakeAdmin(user._id)}
+                      className="btn btn-xs btn-primary"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button className="btn btn-xs btn-error">Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
